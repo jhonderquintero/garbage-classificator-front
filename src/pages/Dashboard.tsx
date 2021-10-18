df0e81027d7c97da5626c4cd952cc5c173666cd3
Jhonder Quintero <jhonderquintero@gmail.com>
2021-10-18 12:13:52 -0400

    Main page / Dashboard components

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useState } from "react";
+import Sidebar from "../components/Sidebar";
 
 export const Dashboard = () => {
+  const [sidebarBoolean, setSidebarBoolean] = useState<boolean>(true);
+
   return (
-    <div>
-      Dashboard
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar
+        sidebarOpen={sidebarBoolean}
+        setSidebarOpen={setSidebarBoolean}
+      />
+      {/* 
+      <Content
+        // Header={
+        //   <WelcomeBanner messageTitle="" messageContent="" SVG={<MainSVG />} />
+        // }
+      /> */}
     </div>
-  )
-}
+  );
+};
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ConfigForm } from "../components/Content/ConfigForm";
+import { Content } from "../components/Content/Content";
+import { GridWrapper } from "../components/GridWrapper";
+import Sidebar from "../components/Sidebar";
+import WelcomeBanner from "../components/WelcomeBanner";
+import { MainSVG } from "../styles/svgs/MainSVG";
 
 export const MainPage = () => {
+  // Design pattern recommended by Kent C. Dodds (Google Expert and creator of React Testing Library)
+  // https://epicreact.dev/one-react-mistake-thats-slowing-you-down/
+
+  const [sidebarBoolean, setSidebarBoolean] = useState(true);
+
   return (
-    <div>
-      Main Page
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar
+        sidebarOpen={sidebarBoolean}
+        setSidebarOpen={setSidebarBoolean}
+      />
+
+      <Content
+        Header={
+          <WelcomeBanner
+            messageTitle="Bienvenido 🤖"
+            messageContent="Para iniciar el proceso de clasificación de residuos sólidos inorgánicos debes llenar el formulario que se encuentra debajo con la información de tus dispositivos."
+            SVG={<MainSVG />}
+          />
+        }
+        CentralContent={
+          <GridWrapper>
+            <ConfigForm />
+          </GridWrapper>
+        }
+      />
     </div>
-  )
-}
+  );
+};
