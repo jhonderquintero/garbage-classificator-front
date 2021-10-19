65c3066df8e5e02024258f326a10db64919bf345
Jhonder Quintero <jhonderquintero@gmail.com>
2021-10-18 22:18:30 -0400

    tailwind config theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      coolGray: colors.coolGray,
+      blue: colors.blue,
+      blueGray: colors.blueGray
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
